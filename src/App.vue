7415b56c63b61f2b61ace249addabd3b7719a4cb
JennyYao <344561707@qq.com>
2024-06-20 14:49:04 +0800

    build:first-commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-button type="primary" size="default">主要按钮</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+        <div class="example-pagination-block">
+            <div class="example-demonstration">When you have few pages</div>
+                <el-pagination layout="prev, pager, next" :total="50" />
+  </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+    import {Elbutton} from 'element-plus'
+    export default {
+        components: { ElButton},
+    }
+<style scoped>
+/* .example-pagination-block + .example-pagination-block {
+  margin-top: 50px;
+} */
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from '@/App.vue'
+
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+
+app.mount('#app')
